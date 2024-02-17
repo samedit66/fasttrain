4697abf2aa2acac50d1c5812ef7f9f983c4c1f91
samedit66 <samedit66@yandex.ru>
2024-02-17 15:51:32 +0300

    Add type annotations and modified signatures
    Add to "batch" callbacks batch index argument, which stores
    current batch index (starting from 0), add to "epoch" callbacks
    epoch index argument, which stores current epoch index (starting from 0)

--- train/callbacks/callback.py
+++ train/callbacks/callback.py
@@ -1,24 +1,72 @@
 from abc import ABC
 
+from train.trainer import Trainer
+
 
 class Callback(ABC):
     
-    def on_train_begin(self, trainer, logs=None): ...
+    def on_train_begin(self,
+                       trainer: Trainer,
+                       logs: dict | None = None
+                       ) -> None:
+        ...
 
-    def on_train_end(self, trainer, logs=None): ...
+    def on_train_end(self,
+                     trainer: Trainer,
+                     logs: dict | None = None
+                     ) -> None:
+        ...
 
-    def on_train_epoch_begin(self, trainer, logs=None): ...
+    def on_epoch_begin(self,
+                       epoch_idx: int,
+                       trainer: Trainer,
+                       logs: dict | None = None
+                       ) -> None:
+        ...
 
-    def on_train_epoch_end(self, trainer, logs=None): ...
+    def on_epoch_end(self,
+                     epoch_idx: int,
+                     trainer: Trainer,
+                     logs: dict | None = None
+                     ) -> None:
+        ...
 
-    def on_train_batch_begin(self, trainer, logs=None): ...
+    def on_train_batch_begin(self,
+                             batch_idx: int,
+                             trainer: Trainer,
+                             logs: dict | None = None
+                             ) -> None:
+        ...
 
-    def on_train_batch_end(self, trainer, logs=None): ...
+    def on_train_batch_end(self,
+                           batch_idx: int,
+                           trainer: Trainer,
+                           logs: dict | None = None
+                           ) -> None:
+        ...
 
-    def on_validation_begin(self, trainer, logs=None): ...
+    def on_validation_begin(self,
+                            trainer: Trainer,
+                            logs: dict | None = None
+                            ) -> None:
+        ...
 
-    def on_validation_end(self, trainer, logs=None): ...
+    def on_validation_end(self,
+                          trainer: Trainer,
+                          logs: dict | None = None
+                          ) -> None:
+        ...
 
-    def on_validation_batch_begin(self, trainer, logs=None): ...
+    def on_validation_batch_begin(self,
+                                  batch_idx: int,
+                                  trainer: Trainer,
+                                  logs: dict | None = None
+                                  ) -> None:
+           ...
 
-    def on_validation_batch_end(self, trainer, logs=None): ...
+    def on_validation_batch_end(self,
+                                batch_idx: int,
+                                trainer: Trainer,
+                                logs: dict | None = None
+                                ) -> None:
+        ...
