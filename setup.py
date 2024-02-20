44f129ecc992de8f379c6c9a1c2968dcfe93bed4
samedit66 <samedit66@yandex.ru>
2024-02-20 14:33:15 +0300

    Add dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='fasttrain',
-    version='0.0.0',
+    version='0.0.1',
     author='samedit66',
     author_email='samedit66@yandex.ru',
     description='Framework for building training loops easier and faster',
@@ -20,6 +20,9 @@ setup(
     install_requires=[
         'torch',
         'tqdm',
+        'numpy',
+        'matplotlib'
+        'scipy',
         ],
     classifiers=[
         'Programming Language :: Python :: 3',
