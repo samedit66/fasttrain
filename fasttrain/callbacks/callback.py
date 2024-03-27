042575cda3a797e87fd1ef34ee2f62da3295d853
samedit66 <samedit66@yandex.ru>
2024-03-27 17:14:18 +0300

    feat: change api of Callback class

--- fasttrain/callbacks/callback.py
+++ fasttrain/callbacks/callback.py
@@ -1,159 +1,184 @@
 from abc import ABC
-
 import collections.abc
+
+import torch
 
 
 class Callback(ABC):
     '''
     Base class used to build new callbacks.
 
-    Callbacks can be passed to `Trainer.train()` method in order to
+    Callbacks can be passed to `Trainer()` constructor in order to
     hook into the various stages of the model training and validation.
 
     To create a custom callback, subclass `fasttrain.callbacks.Callback` and
     override the method associated with the stage of interest.
-
-    To access the trainer, one should use `self.trainer`.
-    To access the training model, one should use `self.model`.
     '''
 
     def __init__(self) -> None:
-        self._trainer = None
-        self._model = None
         self._training_params = None
 
     @property
-    def trainer(self):
-        return self._trainer
-    
-    @trainer.setter
-    def trainer(self, new_trainer):
-        self._trainer = new_trainer
-
-    @property
-    def model(self):
-        return self._model
-    
-    @model.setter
-    def model(self, new_model):
-        self._model = new_model
-
-    @property
-    def training_params(self):
+    def training_params(self) -> collections.abc.Mapping:
+        '''
+        Training parameters. Mapping. Includes the number or epochs and
+        the number of training batches.
+        Example: `self.training_params['num_epochs'], self.training_params['num_batches']`.
+        '''
+        if self._training_params is None:
+            raise RuntimeError("`training_params` are not initialized")
         return self._training_params
     
     @training_params.setter
-    def training_params(self, new_training_params):
-        self._training_params = new_training_params
+    def training_params(self, training_params: collections.abc.Mapping) -> None:
+        '''
+        Sets up the training parameters. Does it only once.
+        Raises exceptions either when trying to set the training params twice
+        or when `training_params` are not a mapping.
+
+        :param training_params: Mapping.
+        :raises RuntimeError: When trying to set the training params twice or more.
+        :raises TypeError: When given `training_params` are not a `Mapping`.
+        '''
+        if self._training_params is not None:
+            raise RuntimeError("`training_params` can't be changed")
+        if not isinstance(training_params, collections.abc.Mapping):
+            raise TypeError("`training_params` must be a mapping")
+        self._training_params = training_params
 
     def on_train_begin(self,
-                       logs: collections.abc.Mapping | None = None
+                       trainer,
+                       model: torch.nn.Module,
                        ) -> None:
         '''
         Called at the beginning of training.
         
-        :param logs: Mapping. Currently no data is passed to this argument,
-            but that may change in the future.
+        :param trainer: Trainer.
+        :param model: Training model.
         '''
 
     def on_train_end(self,
-                     logs: collections.abc.Mapping | None = None
+                     trainer,
+                     model: torch.nn.Module,
+                     logs: collections.abc.Mapping,
                      ) -> None:
         '''
         Called at the end of training.
 
+        :param trainer: Trainer.
+        :param model: Training model.
         :param logs: Mapping. Currently the output of the last call to `on_epoch_end()`
             is passed to this argument, but that may change in the future.
         '''
 
     def on_epoch_begin(self,
+                       trainer,
+                       model: torch.nn.Module,
                        epoch_num: int,
-                       logs: collections.abc.Mapping | None = None
                        ) -> None:
         '''
         Called at the beginning of an epoch.
 
-        :param epoch_num: Current epoch number
-        :param logs: Mapping. Currently no data is passed to this argument,
-            but that may change in the future.
+        :param trainer: Trainer.
+        :param model: Training model.
+        :param epoch_num: Current epoch number.
         '''
 
     def on_epoch_end(self,
+                     trainer,
+                     model: torch.nn.Module,
                      epoch_num: int,
-                     logs: collections.abc.Mapping | None = None
+                     logs: collections.abc.Mapping,
                      ) -> None:
         '''
         Called at the end of an epoch.
 
-        :param epoch_num: Current epoch number
+        :param trainer: Trainer.
+        :param model: Training model.
+        :param epoch_num: Current epoch number.
         :param logs: Mapping, metric results for this training epoch, and for the
             validation epoch if validation is performed. Validation result
-            keys are prefixed with `val_`. Example: `{'loss': 0.2, 'accuracy': 0.7}`.
+            keys are prefixed with `val_`. Example: `{'loss': 0.2, 'accuracy': 0.7, 'val_loss': 0.21, 'val_accuracy': 0.74}`.
         '''
 
     def on_train_batch_begin(self,
+                             trainer,
+                             model: torch.nn.Module,
                              batch_num: int,
-                             logs: collections.abc.Mapping | None = None
                              ) -> None:
         '''
         Called at the beginning of a training batch.
 
-        :param batch_num: Current batch number
-        :param logs: Mapping. Currently no data is passed to this argument,
-            but that may change in the future.
+        :param trainer: Trainer.
+        :param model: Training model.
+        :param batch_num: Current batch number.
         '''
 
     def on_train_batch_end(self,
+                           trainer,
+                           model: torch.nn.Module,
                            batch_num: int,
-                           logs: collections.abc.Mapping | None = None
+                           logs: collections.abc.Mapping,
                            ) -> None:
         '''
         Called at the end of a training batch.
 
-        :param batch_num: Current batch number
+        :param trainer: Trainer.
+        :param model: Training model.
+        :param batch_num: Current batch number.
         :param logs: Mapping. Aggregated metric results up until this batch.
         '''
 
     def on_validation_begin(self,
-                            logs: collections.abc.Mapping | None = None
+                            trainer,
+                            model: torch.nn.Module,
                             ) -> None:
         '''
         Called at the beginning of validation.
 
-        :param logs: Mapping. Currently no data is passed to this argument,
-            but that may change in the future.
+        :param trainer: Trainer.
+        :param model: Training model.
         '''
 
     def on_validation_end(self,
-                          logs: collections.abc.Mapping | None = None
+                          trainer,
+                          model: torch.nn.Module,
+                          logs: collections.abc.Mapping,
                           ) -> None:
         '''
         Called at the end of validation.
 
+        :param trainer: Trainer.
+        :param model: Training model.
         :param logs: Mapping. Currently the output of the last call to
             `on_validation_batch_end()` is passed to this argument
             but that may change in the future.
         '''
 
     def on_validation_batch_begin(self,
+                                  trainer,
+                                  model: torch.nn.Module,
                                   batch_num: int,
-                                  logs: collections.abc.Mapping | None = None
                                   ) -> None:
         '''
         Called at the beginning of a validation batch.
 
-        :param batch_num: Current batch number
-        :param logs: Mapping. Currently no data is passed to this argument,
-            but that may change in the future.
+        :param trainer: Trainer.
+        :param model: Training model.
+        :param batch_num: Current batch number.
         '''
 
     def on_validation_batch_end(self,
+                                trainer,
+                                model: torch.nn.Module,
                                 batch_num: int,
-                                logs: collections.abc.Mapping | None = None
+                                logs: collections.abc.Mapping,
                                 ) -> None:
         '''
         Called at the end of a validation batch.
 
-        :param batch_num: Current batch number
+        :param trainer: Trainer.
+        :param model: Training model.
+        :param batch_num: Current batch number.
         :param logs: Mapping. Aggregated metric results up until this batch. 
         '''
